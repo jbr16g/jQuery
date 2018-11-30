0cd7e153b43b7007c12fa07405c7cd7485dd7f74
Jason Randall <jbr16g@my.fsu.edu>
2018-11-30 23:08:02 +0000

    Second Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/* global $ */
+//displays the next element after the current target
+    function display(event) {
+        $(event.currentTarget).next().fadeIn("slow");
+    }//end of display
+//attach event listener to h3 elements to invoke display function when clicked
+    $("h3").click(display);
